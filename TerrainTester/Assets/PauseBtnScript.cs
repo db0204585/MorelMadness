82f792a92b70c2cbacf98d810f86b834af4300d4
Jevon Wallace <jw0155039@otc.edu>
2018-04-28 11:29:32 -0500

    try again

---
+++ TerrainTester/Assets/PauseBtnScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseBtnScript : MonoBehaviour
+{
+    public static bool PauseMenu = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (PauseMenu)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        PauseMenu = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        PauseMenu = true;
+    }
+
+    public void RestartGameBtn()
+
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGameBtn()
+
+    {
+        Application.Quit();
+    }
+}
---
+++ TerrainTester/Assets/SpawnShroom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnShroom : MonoBehaviour {
+
+    public GameObject shroom;
+    // Use this for initialization
+    void Start () {
+        shroom = GameObject.Find("shroom");
+        int numShrooms = Random.Range(250, 501);
+        for (int i = 0; i < numShrooms; i++)
+        {
+            Vector3 temp = new Vector3(Random.Range(0, 501), Terrain.activeTerrain.SampleHeight(transform.position), Random.Range(0, 501));
+            transform.position = temp;
+            Instantiate(shroom, temp + transform.TransformPoint(0, 0, 0), Quaternion.identity);
+        }
+    }
+	
+
+}
