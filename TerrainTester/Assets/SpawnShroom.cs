dea8b612c45ed6a4ba2ecd9287328c35bbcf6482
ESLINGER <ge0758092@otc.edu>
2018-04-28 07:48:17 -0500

    SpawnShroom.cs is now working, shrooms do not align with terrain.

---
+++ TerrainTester/Assets/SpawnShroom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnShroom : MonoBehaviour {
+    public GameObject shroom;
+    public Terrain Terrain;
+    public LayerMask TerrainLayer;
+
+    // Use this for initialization
+    void Start() {
+        shroom = GameObject.Find("shroom");
+        int numShrooms = Random.Range(250, 501);
+        for (int i = 0; i < numShrooms; i++)
+        {
+
+            Vector3 temp = new Vector3(Random.Range(0, 501), Terrain.activeTerrain.SampleHeight(transform.position), Random.Range(0, 501));
+            transform.position = temp;
+            Instantiate(shroom, temp + transform.TransformPoint(0, 0, 0), Quaternion.identity);
+        }
+    }
+}
