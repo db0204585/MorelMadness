98af80d24956b00b6c630797d7cea337f16f8cc9
ESLINGER <ge0758092@otc.edu>
2018-04-28 11:35:55 -0500

    working spawnShroom

--- TerrainTester/Assets/SpawnShroom.cs
+++ TerrainTester/Assets/SpawnShroom.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnShroom : MonoBehaviour {
-    public GameObject shroom;
+    public GameObject shroom; // create public variable shroom
 
     // Use this for initialization
     void Start() {
-        shroom = GameObject.Find("shroom");
-        int numShrooms = Random.Range(250, 501);
-        for (int i = 0; i < numShrooms; i++)
+        shroom = GameObject.Find("shroom"); // set shroom to shroom.prefab
+        int numShrooms = Random.Range(250, 501); // generate a random number
+        for (int i = 0; i < numShrooms; i++) // loop until random number numShrooms is met
         {
-            Vector3 temp = new Vector3(Random.Range(0, 501), Terrain.activeTerrain.SampleHeight(transform.position), Random.Range(0, 501));
-            transform.position = temp;
+            Vector3 temp = new Vector3(Random.Range(0, 501), Terrain.activeTerrain.SampleHeight(transform.position), Random.Range(0, 501)); // create a vector within the map terrain, with the y axis being on top of the terrain
+            transform.position = temp; //
             Instantiate(shroom, temp + transform.TransformPoint(0, 0, 0), Quaternion.identity);
         }
     }
