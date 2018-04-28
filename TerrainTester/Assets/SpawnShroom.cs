3ce9b9f415590548c8dbc6b423de484d44f8eb70
ESLINGER <ge0758092@otc.edu>
2018-04-28 08:16:21 -0500

    Changed shroom.prefab constraints.

--- TerrainTester/Assets/SpawnShroom.cs
+++ TerrainTester/Assets/SpawnShroom.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class SpawnShroom : MonoBehaviour {
     public GameObject shroom;
-    public Terrain Terrain;
-    public LayerMask TerrainLayer;
 
     // Use this for initialization
     void Start() {
@@ -13,7 +11,6 @@ public class SpawnShroom : MonoBehaviour {
         int numShrooms = Random.Range(250, 501);
         for (int i = 0; i < numShrooms; i++)
         {
-
             Vector3 temp = new Vector3(Random.Range(0, 501), Terrain.activeTerrain.SampleHeight(transform.position), Random.Range(0, 501));
             transform.position = temp;
             Instantiate(shroom, temp + transform.TransformPoint(0, 0, 0), Quaternion.identity);
